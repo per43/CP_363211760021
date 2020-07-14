b28c0157a900b9a40bdca32a061b55f70c816db4
per <nithiwat.w@rmutsvmail.com>
2020-07-14 17:54:35 +0700

    add code 2 lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Lab1.HelloWorld!!!");
         System.out.println("Nithiwat Wechakama!!!");
+        System.out.println("Per!!!");
+        System.out.println("0638132589!!!");
+
 
 
 
