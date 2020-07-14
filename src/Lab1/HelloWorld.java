3302966e87313446da302b3e7eeb6778c8c56405
per <nithiwat.w@rmutsvmail.com>
2020-07-14 17:41:04 +0700

    Initial commit

---
+++ src/Lab1/HelloWorld.java
@@ -0,0 +1,13 @@
+package Lab1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Lab1.HelloWorld!!!");
+        System.out.println("Nithiwat Wechakama!!!");
+
+
+
+
+    }
+}
