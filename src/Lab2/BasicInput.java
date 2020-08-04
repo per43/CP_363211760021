175f99de35b10be93154a669c25078116608a5c4
Per Nithiwat <nithiwat.w@rmutsvmail.com>
2020-08-04 19:01:31 +0700

    Lab 2 : basic input

---
+++ src/Lab2/BasicInput.java
@@ -0,0 +1,39 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class BasicInput {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String name;
+        System.out.println("What is your namr? :");
+        name = scanner.nextLine();
+        System.out.println("Your name is "+name);
+        String A;
+        System.out.println("What is your major? :");
+        A = scanner.nextLine();
+        System.out.println("Your major is "+A);
+        String age;
+        System.out.println("How old are you? :");
+        age = scanner.nextLine();
+        System.out.println("Your age is "+age);
+        String email;
+        System.out.println("What is your email? :");
+        email = scanner.nextLine();
+        System.out.println("Your email is "+email);
+
+
+        //age
+        //email
+
+
+
+
+
+
+    }//main
+}//class
---
+++ src/Lab2/Exercise_Lab2_1.java
@@ -0,0 +1,19 @@
+package Lab2;
+
+import java.util.Scanner;
+
+public class Exercise_Lab2_1 {
+    public static void main (int[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a n1?");
+        int
+
+
+
+
+    }
+
+
+}
