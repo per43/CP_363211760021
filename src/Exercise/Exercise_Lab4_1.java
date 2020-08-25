1aa4cce4f549f659a5abd526a9d4f2c5cbe6e24e
Per Nithiwat <nithiwat.w@rmutsvmail.com>
2020-08-25 16:35:19 +0700

    Lab 3 : ............

---
+++ src/Exercise/Exercise_Lab4_1.java
@@ -0,0 +1,13 @@
+package Exercise;
+
+
+import java.util.Scanner;
+
+public class Exercise_Lab4_1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
---
+++ src/Lab3/Example_While.java
@@ -0,0 +1,23 @@
+
+    package Lab3;
+import java.util.Scanner;
+
+    public class Example_While {
+        public static void main (String[] args){
+            Scanner sc = new Scanner(System.in);
+            int num = 1;
+            while (num !=0) {
+                System.out.println("Enter integer :");
+                num = sc.nextInt();
+            }
+            System.out.println("End.Good Bye.");
+
+
+
+
+        }//main
+
+
+
+    }//class
+
