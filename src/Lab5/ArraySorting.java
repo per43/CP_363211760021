4daf1a2d72c76b85bbc3288b006ce456008e070e
Per Nithiwat <nithiwat.w@rmutsvmail.com>
2020-09-29 15:52:22 +0700

    Exercise_Lab_3_Login0....1

---
+++ src/Lab5/ArraySorting.java
@@ -0,0 +1,14 @@
+package Lab5;
+//sorting คือ การเรียงลำดับข้อมูล
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+
+        int number [] = {13,25,35,40,50};
+
+        System.out.println(Arrays.sort(););
+    }
+}
