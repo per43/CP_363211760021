63651604aa655835434b20e400c7c5d83198cef3
Per Nithiwat <nithiwat.w@rmutsvmail.com>
2020-10-20 16:09:30 +0700

    File new sss

--- src/Lab5/ArraySorting.java
+++ src/Lab5/ArraySorting.java
@@ -1,14 +1,36 @@
 package Lab5;
-//sorting คือ การเรียงลำดับข้อมูล
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collections;
 
+// sorting
 public class ArraySorting {
     public static void main(String[] args) {
 
-        int number [] = {13,25,35,40,50};
+        int number[] = {12, 54, 87, 32, 69};
 
-        System.out.println(Arrays.sort(););
+        // ascending
+        Arrays.sort(number);
+
+        for (int val:number) {
+            System.out.print(val+" ");
+        }
+        System.out.println();
+
+        //descending
+
+        Integer n[] = new Integer[number.length];
+        int i = 0;
+        for (int v:number) {  // int --> Integer
+            n[i++] = Integer.valueOf(v);
+        }
+        Arrays.sort(n, Collections.reverseOrder());
+        System.out.println(Arrays.toString(n));
+
+        int x[][] = {{1,2,3},
+                {4,5,6}};
+        System.out.println(Arrays.toString(x[0]));
+
     }
+
 }
